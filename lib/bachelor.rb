fc4050a5e5bab2cbe3d7ee3d31a23aaad12dfa59
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 16:04:09 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,14 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |season_num, contestants|
+    if season_num == season
+      contestants.each do |cont_hash|
+        if cont_hash["status"] == "Winner"
+          return cont_hash["name"].split(" ")[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
