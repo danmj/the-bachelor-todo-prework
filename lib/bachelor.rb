491f2db2d249b945e1b1c5e5a1fb19f28fcbd142
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 16:04:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,5 +49,16 @@ end
 
 def get_average_age_for_season(data, season)
   # code here
-
+  num_of_cont = 0
+  age_total = 0
+  data.each do |season_num, contestants|
+    if season_num == season
+      contestants.each do |cont_hash|
+        num_of_cont += 1
+        age_total += cont_hash["age"].to_i
+      end
+    end
+  end
+  average = age_total.to_f / num_of_cont.to_f
+  average.to_s[3].to_i >= 5 ? average.ceil : average.to_i
 end
